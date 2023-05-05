47ac7ade8944bc9b04455d4526922a87ae75310d
paulostevo <41132104+paulostevo@users.noreply.github.com>
2023-05-05 08:13:41 -0300

    Generics repositories created

--- Entities/Entidades/UsuarioSistemaFinanceiro.cs
+++ Entities/Entidades/UsuarioSistemaFinanceiro.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace Entities.Entidades
 {
     [Table("UsuarioSistemaFinanceiro")]
-    public class UsusarioSistemaFinanceiro
+    public class UsuarioSistemaFinanceiro
     {
         public int Id { get; set; }
         public string EmailUsuario { get; set; }
